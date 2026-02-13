1a0cda33db5a831d8249b9e3db99f65367daf827
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-02-12 19:03:06 -0600

    feat: implement custom useForm hook for form state management

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,34 @@
+import { useState, type ChangeEvent } from 'react'
+
+type FormState = Record<string, string>
+
+export const useForm = <T extends FormState>(initialFormState: T) => {
+  const [formState, setFormState] = useState<T>(initialFormState)
+
+  // Change the values of the form state when a input element is changing
+  const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value: inputValue, name: inputName } = event.target
+
+    console.log({ inputValue, inputName })
+
+    setFormState((prevFormState) => ({
+      ...prevFormState,
+      [inputName as keyof T]: inputValue,
+    }))
+  }
+
+  // Reset form values
+  const onResetForm = () => {
+    setFormState(initialFormState)
+  }
+
+  return {
+    // Properties
+    ...formState,
+    formState,
+
+    // Methods
+    onInputChange,
+    onResetForm,
+  }
+}
