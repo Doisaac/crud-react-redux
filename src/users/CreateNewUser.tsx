e02669973778edba112d5e30bf19bfca0d3717e1
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-02-13 21:47:13 -0600

    feat: refactor CreateNewUser component with improved form handling and validation

--- src/users/CreateNewUser.tsx
+++ src/users/CreateNewUser.tsx
@@ -1,29 +1,58 @@
-import { Badge } from '@/components/ui/badge'
+import { useState, type SubmitEvent } from 'react'
+import { toast } from 'sonner'
+
 import { Button } from '@/components/ui/button'
 import { Card, CardTitle } from '@/components/ui/card'
+import { Field, FieldDescription } from '@/components/ui/field'
 import { Input } from '@/components/ui/input'
+import { useForm, type InputValidators } from '@/hooks/useForm'
 import { useUsers } from '@/hooks/useUsers'
-import { useEffect, useState, type SubmitEvent } from 'react'
+import { cn } from '@/lib/utils'
+
+const initialFormState = {
+  name: '',
+  github: '',
+  email: '',
+}
+
+const validators: InputValidators<typeof initialFormState> = {
+  name: [
+    (name: string) => name.length > 0,
+    'Name must be at least 1 character',
+  ],
+  github: [
+    (githubUsername: string) => githubUsername.length > 0,
+    'Github username must be at least 1 character',
+  ],
+  email: [
+    (email: string) =>
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email),
+    'Email must be valid',
+  ],
+}
 
 export const CreateNewUser = () => {
+  const {
+    name,
+    github,
+    email,
+    onInputChange,
+    onResetForm,
+    nameValid,
+    githubValid,
+    emailValid,
+  } = useForm(initialFormState, validators)
+
+  const [hasSubmitted, setHasSubmitted] = useState<boolean>(false)
   const { addUser } = useUsers()
-  const [result, setResult] = useState<'ok' | 'ko' | null>(null)
 
   const handleSubmit = (event: SubmitEvent<HTMLFormElement>) => {
     event.preventDefault()
-
-    setResult(null)
-
-    const form = event.target
-    const formData = new FormData(form)
-
-    const name = formData.get('name') as string
-    const email = formData.get('email') as string
-    const github = formData.get('github') as string
+    setHasSubmitted(true)
 
     // Validations
-    if (!name || !email || !github) {
-      return setResult('ko')
+    if (!!nameValid || !!githubValid || !!emailValid) {
+      return
     }
 
     addUser({
@@ -31,46 +60,75 @@ export const CreateNewUser = () => {
       github,
       name,
     })
-    setResult('ok')
-    form.reset()
+
+    toast.success('User successfully added')
+
+    setHasSubmitted(false)
+    onResetForm()
   }
-
-  useEffect(() => {
-    if (result === null) return
-
-    const timer = setTimeout(() => {
-      setResult(null)
-    }, 3000)
-
-    return () => clearTimeout(timer)
-  }, [result])
 
   return (
     <Card className="mt-4 w-full px-2">
       <CardTitle>Create New User</CardTitle>
 
       <form onSubmit={handleSubmit} className="space-y-3">
-        <Input name="name" placeholder="Name" />
-        <Input name="github" placeholder="Github username" />
-        <Input name="email" placeholder="Email address" />
+        <Field>
+          <Input
+            name="name"
+            value={name}
+            onChange={onInputChange}
+            placeholder="Name"
+            className={cn(
+              hasSubmitted &&
+                nameValid &&
+                'transition-colors border-red-500 focus-visible:ring-red-500',
+            )}
+          />
+
+          <FieldDescription className="text-red-600/70">
+            {hasSubmitted && nameValid}
+          </FieldDescription>
+        </Field>
+
+        <Field>
+          <Input
+            name="github"
+            value={github}
+            onChange={onInputChange}
+            placeholder="Github username"
+            className={cn(
+              hasSubmitted &&
+                githubValid &&
+                'transition-colors border-red-500 focus-visible:ring-red-500',
+            )}
+          />
+
+          <FieldDescription className="text-red-600/70">
+            {hasSubmitted && githubValid}
+          </FieldDescription>
+        </Field>
+
+        <Field>
+          <Input
+            name="email"
+            value={email}
+            onChange={onInputChange}
+            placeholder="Email address"
+            className={cn(
+              hasSubmitted &&
+                emailValid &&
+                'transition-colors border-red-500 focus-visible:ring-red-500',
+            )}
+          />
+
+          <FieldDescription className="text-red-600/70">
+            {hasSubmitted && emailValid}
+          </FieldDescription>
+        </Field>
 
         <Button type="submit" variant={'default'}>
           Create User
         </Button>
-
-        <span className="ml-2">
-          {result === 'ok' && (
-            <Badge className="bg-green-50 text-green-700">
-              User successfully created
-            </Badge>
-          )}
-
-          {result === 'ko' && (
-            <Badge className="bg-red-50 text-red-700">
-              Something went wrong. Complete the fields and try again.
-            </Badge>
-          )}
-        </span>
       </form>
     </Card>
   )
